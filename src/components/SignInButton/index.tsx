42af2f5c381cf98fce1eaf6c1c45794d5fe56031
Rvkash <rafael.ieel@hotmail.com>
2021-07-30 16:38:24 -0300

    feat: add signin button github

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,17 @@
+import { FaGithub } from 'react-icons/fa'
+import  styles from './styles.module.scss'
+
+export function SignInButton() {
+  return (
+    <button 
+    type="button"
+    className={styles.sigInButton}
+    >
+      <FaGithub />
+      Sign in with Github
+    </button>
+  )
+}
+
+
+
