accbf5f4101459631571fe840fc43c954f7406e1
Rvkash <rafael.ieel@hotmail.com>
2021-12-23 00:17:41 -0300

    feat: add button subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import  Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
@@ -13,11 +14,12 @@ export default function Home() {
     <main className={styles.contentContainer}>
       <section className={styles.hero}>
         <span> Hey, Welcome! </span>
-        <h1>News about the <span>React</span>world.</h1>
+        <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to the all publication <br />
           <span>for $9,99 Month</span>
         </p>
+        <SubscribeButton/>
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding" />
